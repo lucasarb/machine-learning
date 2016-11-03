5c1365e50c186bc3f82dbf9129fe4e7c55ac0124
Lucas Bessa <alucard0410@gmail.com>
2016-11-03 17:37:40 -0200

    Added comments

--- projects/capstone/whats cooking/final_model.py
+++ projects/capstone/whats cooking/final_model.py
@@ -3,16 +3,20 @@ import time
 import pandas as pd
 import numpy as np
 import re
+#Natural language processing library
 from nltk.stem import WordNetLemmatizer
 
+#Machine learning library
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.preprocessing import LabelEncoder
 from sklearn.svm import LinearSVC
 from sklearn.model_selection import train_test_split
 
+
 train = pd.read_csv('train_modified.csv')
 test = pd.read_csv('test_modified.csv')
 
+#group the test and training dataset
 data = pd.concat([train,test], ignore_index=True)
 
 number_classes = (len(pd.Series.unique(data['cuisine'])))
@@ -21,16 +25,19 @@ lbl_enc = LabelEncoder()
 corpus = data['ingredients_string']
 vectorizer = TfidfVectorizer(stop_words='english', ngram_range = ( 1, 1),analyzer="word", 
                                max_df = .6 , binary=False , token_pattern=r'\w+' , sublinear_tf=False, norm = 'l2')
-
+#create a single count matrix 
 count_matrix = vectorizer.fit_transform(corpus)
-count_matrix_train = count_matrix[:39774]
-count_matrix_test = count_matrix[39774:]
+
+#divide it between the training set and testing set
+count_matrix_train = count_matrix[:train.shape[0]]
+count_matrix_test = count_matrix[train.shape[0]:]
 
 targets = data['cuisine'].iloc[:39774]
 targets = lbl_enc.fit_transform(targets)
 
 x_train, x_test, y_train, y_test = train_test_split(count_matrix_train,targets ,test_size = 0.33, random_state = 42)
 
+#Using the hyperparameters that were obtained from the whats_cooking.py grid search
 clf_SVM = LinearSVC(C=0.5, class_weight=None, dual=True, fit_intercept=True,
      intercept_scaling=1, loss='squared_hinge', max_iter=1000,
      multi_class='ovr', penalty='l2', random_state=None, tol=0.0001,
@@ -41,14 +48,15 @@ clf_SVM.fit(x_train,y_train)
 end = time.time()
 
 
-
 print "Time for Training: {}".format(end - start)
 print "SVM Train score: %.4f" % clf_SVM.score(x_train,y_train)
 print "SVM Test score: %.4f" % clf_SVM.score(x_test,y_test)
 
+#transform the labels into the original names
 predictions = clf_SVM.predict(count_matrix_test)
 predictions = lbl_enc.inverse_transform(predictions)
 
+#format the output to the submission testes on kaggle
 test_output = test
 test_output['cuisine'] = predictions
 test_output = test_output.drop('ingredients_string',axis = 1)
